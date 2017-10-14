4fc1baada74a228dc1be23fa13db8993c2955056
T. A. Ebesu <you@exmaple.com>
2017-10-14 14:23:13 -0700

    AutoEncoder now grabs the indices of the event ids

--- src/aeer/model/auto_encoder.py
+++ src/aeer/model/auto_encoder.py
@@ -12,6 +12,7 @@ import aeer.dataset.event_dataset as ds
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 
+import numpy as np
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.utils import shuffle
 
@@ -21,66 +22,35 @@ class AutoEncoder(object):
     def __init__(self, n_inputs, n_hidden, learning_rate=0.001):
         self.x = tf.placeholder(tf.float32, shape=[None, n_inputs])
 
+        # We need to gather the indices from the matrix where our outputs are
+        self.gather_indices = tf.placeholder(tf.int32, shape=[None, 2])
+
         # Dropout inputs == Masking Noise on inputs
         # By default if we do not feed this in, no dropout will occur
         self.dropout = tf.placeholder_with_default([1.0], None)
-
         n_outputs = n_inputs
 
         # Add some corrpution
+        # TODO: dropout may only be applicable for the sparse input values not
+        # all
         corrupt_inputs = tf.nn.dropout(self.x, self.dropout)
 
         # create hidden layer with default ReLU activation
         hidden = fully_connected(corrupt_inputs, n_hidden)
 
         # create the output layer with no activation function
-        outputs = fully_connected(hidden, n_outputs, activation_fn=None)
+        self.outputs = tf.gather_nd(
+            fully_connected(hidden, n_outputs, activation_fn=None),
+            self.gather_indices)
 
         # square loss
-        self.loss = tf.losses.mean_squared_error(outputs, self.x)
+        # Following CDAE: we set all y=1's
+        self.loss = tf.losses.mean_squared_error(self.outputs,
+                                                 tf.ones_like(self.outputs))
         optimizer = tf.train.AdamOptimizer(learning_rate)
 
         # Train Model
         self.train = optimizer.minimize(self.loss)
-
-
-def fit():
-    # get the number of event count
-    n_inputs = ds.get_event_count()
-    n_hidden = 50
-    n_outputs = n_inputs
-    learning_rate = 0.01
-
-    x = tf.placeholder(tf.float32, shape=[None, n_inputs])
-    # create hidden layer with default ReLU activation
-    hidden = fully_connected(x, n_hidden);
-    # create the output layer with no activation function
-    outputs = fully_connected(hidden, n_outputs, activation_fn=None);
-    # square loss
-    reconstruction_loss = tf.reduce_mean(tf.square(outputs - x))
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    training_op = optimizer.minimize(reconstruction_loss)
-
-    # evaluate
-    # tf.nn.in_top_k
-    # can only use log loss or cross-entropy loss
-    # value range has to be -1 to 1 for
-
-    init = tf.global_variables_initializer()
-    # save the model
-    saver = tf.train.Saver()
-
-    n_epochs = 400
-    with tf.Session() as sess:
-        init.run()
-        for epoch in range(n_epochs):
-            # feed the training data
-            # additive gaussian noise or multiplicative mask-out/drop-out noise
-            sess.run(training_op)
-
-        # save_path = saver.save(sess, "/path to file")
-
-
 
 def main():
     event_data = ds.EventData(ds.chicago_file_name)
@@ -90,6 +60,8 @@ def main():
     # Convert the sparse event indices to a dense vector
     mlb = MultiLabelBinarizer()
     mlb.fit([events])
+    # We need this to get the indices of events
+    class_to_index = dict(zip(mlb.classes_, range(len(mlb.classes_))))
 
     model = AutoEncoder(len(events), 50, learning_rate=0.001)
     train_x, test_x, train_y, test_y = event_data.split_dataset()
@@ -110,14 +82,16 @@ def main():
         :param mlb: sklearn.MultiLabelBinarizer fitted with the event data
         :returns: np.array of values
         """
-        item_ids = [df.eventId[df.memberId == uid].unique() for uid in user_ids]
-        return mlb.transform(item_ids)
+        item_ids = [df.eventId[df.memberId == uid].unique() for uid in user_ids
+                    if len(df.eventId[df.memberId == uid].unique()) > 0]
+        return mlb.transform(item_ids), item_ids
 
     init = tf.global_variables_initializer()
 
     n_epochs = 400
     batch_size = 64
     batches_per_iteration = int(len(users) / batch_size)
+
 
     with tf.Session() as sess:
         init.run()
@@ -129,9 +103,19 @@ def main():
                 # Batch indices
                 lo = batch_size * idx
                 hi = lo + batch_size
+
+                x, item_ids = get_batch(train_x, users[lo:hi], mlb)
+
+                # We only compute loss on events we used as inputs
+                gather_indices = []
+                for row_index, ids in enumerate(item_ids):
+                    gather_indices.extend([[row_index, class_to_index[iid]]
+                                           for iid in ids])
+
                 # Get a batch of data
                 batch_loss, _ = sess.run([model.loss, model.train], {
-                    model.x: get_batch(train_x, users[lo:hi], mlb),
+                    model.x: x,
+                    model.gather_indices: gather_indices,
                     model.dropout: 0.5 # Dropout = Some masking noise
                 })
                 epoch_loss += batch_loss
