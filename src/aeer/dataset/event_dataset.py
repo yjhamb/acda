c8792a7df758d300f96a4ba049c6d78cda70689a
T. A. Ebesu <you@exmaple.com>
2017-11-04 20:23:40 -0700

    added latent factor autoencoder, subclassed EventData to get user group data; also added abstractions"

--- src/aeer/dataset/event_dataset.py
+++ src/aeer/dataset/event_dataset.py
@@ -8,14 +8,16 @@ import sklearn.model_selection as ms
 
 from scipy import sparse
 from scipy.stats import bernoulli
+from sklearn.preprocessing import MultiLabelBinarizer
 
 ny_file_name = "../../../dataset/rsvp_ny.csv"
 sfo_file_name = "../../../dataset/rsvp_sfo.csv"
 dc_file_name = "../../../dataset/rsvp_dc.csv"
 chicago_file_name = "../../../dataset/rsvp_chicago.csv"
 
+
 class EventData(object):
-    
+
     def __init__(self, file_name):
         self.events = pd.read_csv(file_name)
         self.events.loc[self.events['rsvpRating'] == -1, 'rsvpRating'] = 1
@@ -23,69 +25,69 @@ class EventData(object):
         events_sorted = self.events.sort_values(['eventTime'], ascending=True)
         x = events_sorted.drop(['rsvpRating'], axis=1)
         y = events_sorted[['rsvpRating']]
-    
+
         # perform the train-test split
         self.train_x, self.test_x, self.train_y, self.test_y = ms.train_test_split(x, y, test_size=0.2, random_state=42)
-        
-    
+
+
     def get_users(self):
         return self.events.memberId.unique()
 
-    
+
     def get_events(self):
         return self.events.eventId.unique()
-    
-    
+
+
     def get_venues(self):
         return self.events.venueId.unique()
-    
-    
+
+
     def get_groups(self):
         return self.events.groupId.unique()
 
 
     def get_train_users(self):
         return self.train_x.memberId.unique()
-    
-    
+
+
     def get_test_users(self):
         return self.test_x.memberId.unique()
 
 
     def get_train_events(self):
         return self.train_x.eventId.unique()
-    
-    
+
+
     def get_test_events(self):
         return self.test_x.eventId.unique()
-    
-    
+
+
     def get_user_unique_test_events(self, user_id):
         return self.test_x.eventId[self.test_x.memberId == user_id].unique()
 
-    
+
     def get_user_train_events(self, user_id, class_to_index, negative_count, corrupt_ratio):
         """
         Calls the get_user_events method with the training data
         """
         return self.get_user_events(user_id, self.train_x, class_to_index, negative_count, corrupt_ratio)
 
-    
+
     def get_user_test_events(self, user_id, class_to_index):
         """
         Calls the get_user_events method with the test data
         """
         return self.get_user_events(user_id, self.test_x, class_to_index)
 
-    
+
     def get_user_events(self, user_id, df, class_to_index, negative_count=0, corrupt_ratio=0):
         """
-        This will get a single users events (training or test based on input parameter). 
-        We encode each user with a k-hot encoding, where a 1 if they have rated the item. 
-        We then sample negative items they have not observed, if neg_ratio > 0. 
-        Negative items have a target of 0 and positives 1. 
+        This will get a single users events (training or test based on input parameter).
+        We encode each user with a k-hot encoding, where a 1 if they have rated the item.
+        We then sample negative items they have not observed, if neg_ratio > 0.
+        Negative items have a target of 0 and positives 1.
         We finally corrupt all the encoded user vectors, if the corrupt_ratio > 0.
-    
+
         :param user_id: user id in dataframe
         :param df: the dataframe for training or test
         :param class_to_index: dictionary that maps item ids to indices
@@ -93,43 +95,43 @@ class EventData(object):
         :param corrupt_ratio: float, [0, 1] the probability of corrupting samples
         :returns: Encoded User Vector, Y Target, item ids
         """
-    
+
         event_count = len(class_to_index)
-    
+
         # Get all positive items
         positives = [class_to_index[i] for i in df.eventId[df.memberId == user_id].unique()]
-    
+
         # Sample negative items
         if negative_count > 0:
             negatives = [self.sample_negative(positives, event_count) for _ in range(negative_count)]
         else:
             negatives = []
-            
+
         input_count = len(positives) + len(negatives)
-    
+
         # X vector for a single user
         # Duplicate input count times
         x_data = [1.0] * input_count
-    
+
         # Indices for the items
         cols = positives + negatives
         rows = []
         for i in range(input_count):
             rows.extend([i] * input_count)
-    
+
         x = sparse.coo_matrix((x_data * input_count,
                                (rows, cols * input_count)),
                               shape=(input_count, event_count),
                               dtype=np.float32)
-    
+
         # Negative targets are 0, positives are 1
         y_targets = np.zeros(input_count, dtype=np.float32)
         y_targets[:len(positives)] = 1.0
-    
+
         # Sparse Matrix; directly take the data and corrupt it
         if corrupt_ratio > 0:
             x.data = self.corrupt_input(x.data, corrupt_ratio).astype(np.float32)
-        
+
         return x, y_targets, cols
 
 
@@ -139,22 +141,22 @@ class EventData(object):
         """
         return self.get_user_events_with_context(user_id, self.train_x, user_group_data, event_class_to_index, group_class_to_index, negative_count, corrupt_ratio)
 
-    
+
     def get_user_test_events_with_context(self, user_id, user_group_data, event_class_to_index, group_class_to_index):
         """
         Calls the get_user_events method with the test data
         """
         return self.get_user_events_with_context(user_id, self.test_x, user_group_data, event_class_to_index, group_class_to_index)
 
-    
+
     def get_user_events_with_context(self, user_id, df, user_group_data, event_class_to_index, group_class_to_index, negative_count=0, corrupt_ratio=0):
         """
-        This will get a single users events (training or test based on input parameter). 
-        We encode each user with a k-hot encoding, where a 1 if they have rated the item. 
-        We then sample negative items they have not observed, if neg_ratio > 0. 
-        Negative items have a target of 0 and positives 1. 
+        This will get a single users events (training or test based on input parameter).
+        We encode each user with a k-hot encoding, where a 1 if they have rated the item.
+        We then sample negative items they have not observed, if neg_ratio > 0.
+        Negative items have a target of 0 and positives 1.
         We finally corrupt all the encoded user vectors, if the corrupt_ratio > 0.
-    
+
         :param user_id: user id in dataframe
         :param df: the dataframe for training or test
         :param event_class_to_index: dictionary that maps event ids to indices
@@ -163,15 +165,15 @@ class EventData(object):
         :param corrupt_ratio: float, [0, 1] the probability of corrupting samples
         :returns: Encoded User Vector, Y Target, item ids
         """
-    
+
         event_count = len(event_class_to_index)
         group_count = len(group_class_to_index)
-    
+
         # Get all positive items
         positive_samples = df[df.memberId == user_id]
         positive_events = [event_class_to_index[i] for i in positive_samples.eventId.unique()]
         positive_groups = [group_class_to_index[i] for i in positive_samples.groupId.unique()]
-    
+
         # Sample negative items
         negative_groups = []
         negative_samples = []
@@ -182,32 +184,32 @@ class EventData(object):
             negative_groups.extend(negative_samples.groupId.unique())
         else:
             negative_events = []
-            
+
         input_count = len(positive_events) + len(negative_events)
-    
+
         # X vector for a single user
         # Duplicate input count times
         x_data = [1.0] * input_count
-        
+
         # Indices for the items
         cols = positive_events + negative_events
         rows = []
         for i in range(input_count):
             rows.extend([i] * input_count)
-    
+
         x = sparse.coo_matrix((x_data * input_count,
                                (rows, cols * input_count)),
                               shape=(input_count, event_count),
                               dtype=np.float32)
-    
+
         # Negative targets are 0, positives are 1
         y_targets = np.zeros(input_count, dtype=np.float32)
         y_targets[:len(positive_events)] = 1.0
-    
+
         # Sparse Matrix; directly take the data and corrupt it
         if corrupt_ratio > 0:
             x.data = self.corrupt_input(x.data, corrupt_ratio).astype(np.float32)
-        
+
         input_group_count = len(positive_groups) + len(negative_groups)
         # create input data vector for groups based on membership
         x_group_data = []
@@ -216,24 +218,24 @@ class EventData(object):
                 x_group_data.extend(1.0)
             else:
                 x_group_data.extend(0.0)
-                
+
         # extend for negative samples
         for group_id in negative_samples.groupId.unique():
             if user_group_data.is_user_in_group(user_id, group_id):
                 x_group_data.extend(1.0)
             else:
                 x_group_data.extend(0.0)
-        
+
         # Indices for the items
         group_cols = positive_groups + negative_groups
         group_rows = []
         group_rows.extend(x_group_data * input_group_count)
-    
+
         x_group = sparse.coo_matrix((x_group_data * input_group_count,
                                (group_rows, group_cols * input_group_count)),
                               shape=(input_group_count, group_count),
                               dtype=np.float32)
-        
+
         # hstack both the event and group matrices
         input_x = np.vstack(x, x_group)
         return input_x, y_targets, cols
@@ -241,7 +243,7 @@ class EventData(object):
 
     def sample_negative(self, pos_item_map, max_items):
         """Sample uniformly items that are not observed
-    
+
         :param pos_item_map: set/list, listing all of the users observed items
         :param max_items: int, item count
         :returns: int negative item id
@@ -255,8 +257,8 @@ class EventData(object):
 
     def sample_negative_on_context(self, user_id, pos_item_map, max_items):
         """Sample uniformly items that are not observed
-    
-        :param user_id: user id for which the 
+
+        :param user_id: user id for which the
         :param pos_item_map: set/list, listing all of the users observed items
         :param max_items: int, item count
         :returns: int negative item id
@@ -283,18 +285,18 @@ class EventData(object):
         x = x * mask
         # Re-scale values
         return x * scale
-   
-    
+
+
     def _get_batch(self, df, user_ids, mlb):
         """
         This method creates a single vector for each user where all of their
         events are set to a 1, otherwise its a 0.
-    
+
         In this case, this user has observed events: 1 and 4
         [1, 0, 0, 1, 0]
-    
+
         TODO: Should probably abstract this out somewhere
-    
+
         :param df: pd.DataFrame of test/train
         :param user_ids: list of user ids, this will query the dataframe
         :param mlb: sklearn.MultiLabelBinarizer fitted with the event data
@@ -304,8 +306,81 @@ class EventData(object):
                     if len(df.eventId[df.memberId == uid].unique()) > 0]
         return mlb.transform(item_ids), item_ids
 
-    
-       
+
+class EventDataWithGroups(EventData):
+
+    """Inherits EventData"""
+    def __init__(self, file_name):
+        super(EventDataWithGroups, self).__init__(file_name)
+        self._n_users = len(self.get_users())
+        self._n_events = len(self.get_events())
+        self._n_groups = len(self.get_groups())
+        self._n_venues = len(self.get_venues())
+
+        # Convert the sparse event indices to a dense vector
+        mlb = MultiLabelBinarizer()
+        mlb.fit([self.get_events()])
+        # We need this to get the indices of events
+        self._event_class_to_index = dict(zip(mlb.classes_, range(len(mlb.classes_))))
+
+        # Convert the sparse group indices to a dense vector
+        mlb_group = MultiLabelBinarizer()
+        mlb_group.fit([self.get_groups()])
+        # We need this to get the indices of events
+        self._group_class_to_index = dict(zip(mlb_group.classes_, range(len(mlb_group.classes_))))
+
+
+    @property
+    def n_users(self):
+        """Return the number of users in the dataset"""
+        return self._n_users
+
+    @property
+    def n_events(self):
+        """Return the number of events in the dataset"""
+        return self._n_events
+
+    @property
+    def n_groups(self):
+        """Return the number of groups in the dataset"""
+        return self._n_groups
+
+    @property
+    def n_venues(self):
+        """Return the number of venues in the dataset"""
+        return self._n_venues
+
+    def get_user_test_event_index(self, user_id):
+        """
+        Get the converted index of user events
+        """
+        unique_user_test_events = self.test_x.eventId[self.test_x.memberId == user_id].unique()
+        return [self._event_class_to_index[i]
+                            for i in unique_user_test_events]
+
+    def get_user_test_events_with_group(self, user_id):
+        """
+        Get test event data with the groups for the user
+        """
+        groups = [self._group_class_to_index[i] for i in
+                  self.test_x[self.test_x.memberId == user_id].groupId.unique()]
+        # * is to unpack it and return a flat list of elements rather than a
+        # nested one
+        return (*self.get_user_events(user_id, self.test_x, self._event_class_to_index)), groups
+
+    def get_user_train_events_with_group(self, user_id, negative_count, corrupt_ratio):
+        """
+        Get train user events with the group for the user with negative counts
+        and corruption ratio
+        """
+        groups = [self._group_class_to_index[i] for i in
+                  self.train_x[self.train_x.memberId == user_id].groupId.unique()]
+        # * is to unpack it and return a flat list of elements rather than a
+        # nested one
+        return (*self.get_user_events(user_id, self.train_x, self._event_class_to_index,
+                                          negative_count, corrupt_ratio)), groups
+
+
 def main():
     print("main method")
     print("Current Directory: ", os.getcwd())
---
+++ src/aeer/model/latent_auto_encoder.py
@@ -0,0 +1,192 @@
+'''
+Denoising AutoEncoder Implementation that incorporates latent factors for
+contextual group and venue data
+'''
+import argparse
+import os
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# parser.add_argument('-g', '--gpu', help='set gpu device number 0-3', type=str, default='3')
+parser.add_argument('-e', '--epochs', help='Number of epochs', type=int, default=30)
+parser.add_argument('-s', '--size', help='Number of hidden layer',
+                    type=int, default=50)
+parser.add_argument('-n', '--neg_count', help='Number of negatives', type=int,
+                    default=4)
+parser.add_argument('-c', '--corrupt', help='Corruption ratio', type=float,
+                    default=0.1)
+FLAGS = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+
+import aeer.dataset.event_dataset as ds
+import aeer.dataset.user_group_dataset as user_group_ds
+import tensorflow as tf
+from tensorflow.contrib.layers import fully_connected
+
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.utils import shuffle
+
+
+class LatentFactorAutoEncoder(object):
+
+    def __init__(self, n_inputs, n_hidden, n_outputs, n_groups, #n_venues,
+                 learning_rate=0.001):
+
+        self.x = tf.placeholder(tf.float32, shape=[None, n_inputs])
+        self.group_id = tf.placeholder(tf.int32, shape=[None])
+        #self.venue_id = tf.placeholder(tf.int32, shape=[None])
+
+        # We need to gather the indices from the matrix where our outputs are
+        self.gather_indices = tf.placeholder(tf.int32, shape=[None, 2])
+
+        self.y = tf.placeholder(tf.float32, shape=[None])
+
+        # Weights
+        W = tf.get_variable('W', shape=[n_inputs, n_hidden])
+        b = tf.get_variable('Bias', shape=[n_hidden])
+
+        # Uniform Initialization U(-eps, eps)
+        eps = 0.01
+
+        # venue_bias = tf.get_variable('VenueBias', shape=[n_venues, n_hidden],
+        #                              initializer=tf.random_uniform_initializer(-eps, eps))
+        group_bias = tf.get_variable('GroupBias', shape=[n_groups, n_hidden],
+                                     initializer=tf.random_uniform_initializer(-eps, eps))
+
+        # We lookup a bias for each
+        # self.venue_factor = tf.nn.embedding_lookup(venue_bias, self.venue_id,
+        #                                            name='VenueLookup')
+        self.group_factor = tf.nn.embedding_lookup(group_bias, self.group_id,
+                                                   name='GroupLookup')
+        # Sum all group factors, then make it a vector so it will broadcast
+        # and add it to all instances
+        group_factor = tf.squeeze(tf.reduce_sum(self.group_factor, axis=0))
+
+        # Wx + b + venue + user groups
+        preactivation = tf.nn.xw_plus_b(self.x, W, b) + group_factor
+        # self.venue_factor +
+
+        hidden = tf.nn.relu(preactivation)
+
+        # add weight regularizer
+        # self.reg_scale = 0.01
+        # self.weights_regularizer = tf.nn.l2_loss(W, "weight_loss")
+        #self.reg_loss = tf.reduce_sum(tf.abs(W))
+
+        # create the output layer with no activation function
+        self.outputs = fully_connected(hidden, n_outputs, activation_fn=None)
+
+        self.targets = tf.gather_nd(self.outputs, self.gather_indices)
+
+        self.actuals = tf.placeholder(tf.int32, shape=[None])
+
+        # evaluate top k wrt outputs and actuals
+        self.top_k = tf.nn.in_top_k(self.outputs, self.actuals, k=10)
+
+        # square loss
+        #self.loss = tf.losses.mean_squared_error(self.targets, self.y) + self.reg_scale * self.weights_regularizer
+        self.loss = tf.losses.mean_squared_error(self.targets, self.y)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        # Train Model
+        self.train = optimizer.minimize(self.loss)
+
+
+def main():
+    n_epochs = FLAGS.epochs
+    n_hidden = FLAGS.size
+    NEG_COUNT = FLAGS.neg_count
+    CORRUPT_RATIO = FLAGS.corrupt
+
+    event_data = ds.EventDataWithGroups(ds.chicago_file_name)
+    users = event_data.get_users()
+    events = event_data.get_events()
+    groups = event_data.get_groups()
+
+
+    # Convert the sparse event indices to a dense vector
+    # mlb = MultiLabelBinarizer()
+    # mlb.fit([events])
+    # # We need this to get the indices of events
+    # event_class_to_index = dict(zip(mlb.classes_, range(len(mlb.classes_))))
+
+    # # Convert the sparse group indices to a dense vector
+    # mlb_group = MultiLabelBinarizer()
+    # mlb_group.fit([groups])
+    # # We need this to get the indices of events
+    # group_class_to_index = dict(zip(mlb_group.classes_, range(len(mlb_group.classes_))))
+
+    n_inputs = event_data.n_events
+    n_groups = event_data.n_groups
+    n_outputs = event_data.n_events
+
+    model = LatentFactorAutoEncoder(n_inputs, n_hidden, n_outputs, n_groups,
+                                    learning_rate=0.001)
+
+    init = tf.global_variables_initializer()
+
+    tf_config = tf.ConfigProto(
+        gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.25,
+                                  allow_growth=True))
+
+    with tf.Session(config=tf_config) as sess:
+        init.run()
+        for epoch in range(n_epochs):
+            # additive gaussian noise or multiplicative mask-out/drop-out noise
+            epoch_loss = 0.0
+            users = shuffle(users)
+
+            for user_id in users:
+                x, y, item, group_id = event_data.get_user_train_events_with_group(user_id, NEG_COUNT, CORRUPT_RATIO)
+
+                # We only compute loss on events we used as inputs
+                # Each row is to index the first dimension
+                gather_indices = list(zip(range(len(y)), item))
+
+                # Get a batch of data
+                batch_loss, _ = sess.run([model.loss, model.train], {
+                    model.x: x.toarray().astype(np.float32),
+                    model.gather_indices: gather_indices,
+                    model.group_id: group_id,
+                    model.y: y
+                })
+
+                epoch_loss += batch_loss
+
+            print("Epoch {:,}/{:<10,} Loss: {:,.6f}".format(epoch, n_epochs,
+                                                            epoch_loss))
+
+            # evaluate the model on the test set
+            test_users = event_data.get_test_users()
+            precision = 0
+            valid_test_users = 0
+            for user_id in test_users:
+                # check if user was present in training data
+                train_users = event_data.get_train_users()
+                if user_id in train_users:
+                    valid_test_users = valid_test_users + 1
+                    #unique_user_test_events = event_data.get_user_unique_test_events(user_id)
+                    test_event_index = event_data.get_user_test_event_index(user_id)
+                    #[event_data._event_class_to_index[i] for i in unique_user_test_events]
+
+                    x, _, _, group_id = event_data.get_user_test_events_with_group(user_id)
+
+                    # evaluate the model using the actuals
+                    top_k_events = sess.run(model.top_k, {
+                        model.x: x.toarray().astype(np.float32),
+                        model.actuals: test_event_index,
+                        model.group_id: group_id,
+                    })
+
+                    precision = precision + np.sum(top_k_events)
+
+            avg_precision = 0
+            if (valid_test_users > 0):
+                avg_precision = precision / valid_test_users
+
+            print("Precision: {:,.6f}".format(avg_precision))
+
+
+
+if __name__ == '__main__':
+    main()
