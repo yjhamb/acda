8273973aa202d3396c9d016b2d6bc9b1c53f2c0a
yjhamb <yjhamb@apple.com>
2017-11-11 18:38:40 -0800

    attempt to fix dataset

--- src/aeer/dataset/event_dataset_util.py
+++ src/aeer/dataset/event_dataset_util.py
@@ -8,14 +8,24 @@ ny_file_name = "../../../dataset/rsvp_ny.csv"
 sfo_file_name = "../../../dataset/rsvp_sfo.csv"
 dc_file_name = "../../../dataset/rsvp_dc.csv"
 chicago_file_name = "../../../dataset/rsvp_chicago.csv"
-chicago_file_name_new = "../../../dataset/rsvp_chicago_new.csv"
+chicago_file_name_new = "Users/yjhamb/Projects/aeer/dataset/rsvp_chicago_new.csv"
 
 def update_rsvp_data(file_name):
-    events = pd.read_csv(file_name)
+    events = pd.read_csv(file_name, dtype = {'memberId': str,'memberLat': str,
+                                            'memberLon': str,'eventId': str,
+                                            'eventTime': str,'eventYesRSVPCount': str,
+                                            'groupId': str,'venueId': str,
+                                            'venueLat': str,'venueLon': str,
+                                            'rsvpTime': str,'rsvpResponse': str,
+                                            'rsvpRating': str,'eventDay': str,
+                                            'eventPeriod': str})
+    print(events.dtypes)
     print(events['rsvpRating'].unique())
-    events.loc[events['rsvpRating'] == -1, 'rsvpRating'] = 1
+    #events.loc[events['rsvpResponse'] == 'watching', 'rsvpRating'] = 1
+    #events.loc[events['rsvpResponse'] == 'waitlist', 'rsvpRating'] = 1
     print(events['rsvpResponse'].unique())
-    #pd.DataFrame.to_csv(chicago_file_name_new)
+    print(events['rsvpRating'].unique())
+    pd.DataFrame.to_csv(chicago_file_name_new, index=False)
 
 def main():
     print("Main method")
