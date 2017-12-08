cd7bbb92f50b9c2f80c80bb2a2a3dc48d6c7873b
yjhamb <yjhamb@apple.com>
2017-12-08 14:58:42 -0800

    training and evaluating in step for each user

--- src/aeer/model/latent_auto_encoder.py
+++ src/aeer/model/latent_auto_encoder.py
@@ -344,7 +344,140 @@ def main():
             print(f"NDCG@5:      {avg_ndcg_5:>10.6f}       NDCG@10:      {avg_ndcg_10:>10.6f}")
             print()
 
+
+def execute():
+    n_epochs = FLAGS.epochs
+    n_hidden = FLAGS.size
+    NEG_COUNT = FLAGS.neg_count
+    CORRUPT_RATIO = FLAGS.corrupt
+
+    event_data = ds.EventData(ds.rsvp_chicago_file, ug_dataset.user_group_chicago_file)
+    users = event_data.get_train_users()
+
+    n_inputs = event_data.n_events
+    n_groups = event_data.n_groups
+    n_outputs = event_data.n_events
+    n_venues = event_data.n_venues
+
+    # We set to None to turn off the group/venue latent factors
+    if FLAGS.nogroup:
+        print("Disabling Group Latent Factor")
+        n_groups = None
+
+    if FLAGS.novenue:
+        print("Disabling Venue Latent Factor")
+        n_venues = None
+
+    model = LatentFactorAutoEncoder(n_inputs, n_hidden, n_outputs, n_groups, n_venues,
+                                    FLAGS.hidden_fn, FLAGS.output_fn,
+                                    learning_rate=0.001)
+
+    init = tf.global_variables_initializer()
+    init_local = tf.local_variables_initializer()
+
+    tf_config = tf.ConfigProto(
+        gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.25,
+                                  allow_growth=True))
+
+    with tf.Session(config=tf_config) as sess:
+        init.run()
+        init_local.run()
+        users = shuffle(users)
+
+        precision = []
+        recall = []
+        mean_avg_prec = []
+        ndcg = []
+        eval_at = [5, 10]
+        valid_test_users = 0
+        for user_id in users:
+            epoch_loss = 0.0
+            print("Training the model for user: ", user_id)
+            for epoch in range(n_epochs):
+                x, y, item = event_data.get_user_train_events(
+                                                    user_id, NEG_COUNT, CORRUPT_RATIO)
+
+                group_ids = event_data.get_user_train_groups(user_id)
+                venue_ids = event_data.get_user_train_venues(user_id)
+
+                # We only compute loss on events we used as inputs
+                # Each row is to index the first dimension
+                gather_indices = list(zip(range(len(y)), item))
+
+                # Get a batch of data
+                batch_loss, _ = sess.run([model.loss, model.train], {
+                    model.x: x.toarray().astype(np.float32),
+                    model.gather_indices: gather_indices,
+                    model.group_id: group_ids,
+                    model.venue_id: venue_ids,
+                    model.y: y
+                })
+                epoch_loss += batch_loss
+            
+            print("Epoch: {:>16}       Loss: {:>10,.6f}".format("%s/%s" % (epoch, n_epochs),
+                                                            epoch_loss))
+
+            # evaluate the model on the test set
+            cv_users = event_data.get_cv_users()
+    
+            if user_id in cv_users:
+                print("Evaluating the model for user: ", user_id)
+                # check if user was present in training data
+                valid_test_users = valid_test_users + 1
+                test_event_index = event_data.get_user_cv_event_index(user_id)
+
+                x, _, _ = event_data.get_user_train_events(user_id, 0, 0)
+                group_ids = event_data.get_user_train_groups(user_id)
+                venue_ids = event_data.get_user_train_venues(user_id)
+                # Compute score
+                score = sess.run(model.outputs, {
+                    model.x: x.toarray().astype(np.float32),
+                    model.group_id: group_ids,
+                    model.venue_id: venue_ids,
+                })[0] # We only do one sample at a time, take 0 index
+
+                # Sorted in ascending order, we then take the last values
+                index = np.argsort(score)
+
+                # Number of test instances
+                preck = []
+                recallk = []
+                mapk = []
+                ndcgk = []
+                for k in eval_at:
+                    preck.append(precision_at_k(index, test_event_index, k))
+                    recallk.append(recall_at_k(index, test_event_index, k))
+                    mapk.append(map_at_k(index, test_event_index, k))
+                    ndcgk.append(ndcg_at_k(index, test_event_index, k))
+
+                precision.append(preck)
+                recall.append(recallk)
+                mean_avg_prec.append(mapk)
+                ndcg.append(ndcgk)
+    
+        if valid_test_users > 0:
+            # Unpack the lists zip(*[[1,2], [3, 4]]) => [1,3], [2,4]
+            avg_precision_5, avg_precision_10 = zip(*precision)
+            avg_precision_5, avg_precision_10 = np.mean(avg_precision_5), np.mean(avg_precision_10)
+
+            avg_recall_5, avg_recall_10 = zip(*recall)
+            avg_recall_5, avg_recall_10 = np.mean(avg_recall_5), np.mean(avg_recall_10)
+
+            avg_map_5, avg_map_10 = zip(*mean_avg_prec)
+            avg_map_5, avg_map_10 = np.mean(avg_map_5), np.mean(avg_map_10)
+
+            avg_ndcg_5, avg_ndcg_10 = zip(*ndcg)
+            avg_ndcg_5, avg_ndcg_10 = np.mean(avg_ndcg_5), np.mean(avg_ndcg_10)
+
+            # Directly access variables
+            print(f"Precision@5: {avg_precision_5:>10.6f}       Precision@10: {avg_precision_10:>10.6f}")
+            print(f"Recall@5:    {avg_recall_5:>10.6f}       Recall@10:    {avg_recall_10:>10.6f}")
+            print(f"MAP@5:       {avg_map_5:>10.6f}       MAP@10:       {avg_map_10:>10.6f}")
+            print(f"NDCG@5:      {avg_ndcg_5:>10.6f}       NDCG@10:      {avg_ndcg_10:>10.6f}")
+            print()
+
+
 if __name__ == '__main__':
     FLAGS = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
-    main()
+    execute()
