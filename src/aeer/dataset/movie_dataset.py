cf48a02a89602e03042fdee803df38bb213971ac
yjhamb <yjhamb@apple.com>
2018-02-12 17:18:17 -0800

    movielens related code

---
+++ src/aeer/dataset/movie_dataset.py
@@ -0,0 +1,281 @@
+'''
+Functions that operate on the movielens dataset
+'''
+import os
+import pandas as pd
+import numpy as np
+import sklearn.model_selection as ms
+
+from scipy import sparse
+from scipy.stats import bernoulli
+from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
+
+RATINGS_CONTEXT_FILE = "../../../dataset/movielens/rating_context.csv"
+
+class MovieRatingsData(object):
+
+    def __init__(self):
+        self.ratings = pd.read_csv(RATINGS_CONTEXT_FILE)
+
+        # perform the train-test split
+        self.train_ratings, self.test_ratings = ms.train_test_split(self.ratings, test_size=0.2, random_state=42)
+
+        # split again to generate CV set
+        self.train_ratings, self.cv_ratings = ms.train_test_split(self.train_ratings, test_size=0.25, random_state=42)
+
+        print("Total Ratings:", len(self.ratings))
+        print("Train Ratings:", len(self.train_ratings))
+        print("CV Ratings:", len(self.cv_ratings))
+        print("Test Ratings:", len(self.test_ratings))
+        
+        self._n_users = len(self.get_users())
+        self._n_movies = len(self.get_movies())
+        self._n_genres = len(self.get_genres())
+        
+        print("Total Users:", self._n_users)
+        print("Total Movies:", self._n_movies)
+        print("Total Genres:", self._n_genres)
+
+        # Convert the sparse event indices to a dense vector
+        self._mlb_movie = MultiLabelBinarizer(sparse_output=True)
+        self._mlb_movie.fit([self.get_movies()])
+        # We need this to get the indices of events
+        self._movie_class_to_index = dict(zip(self._mlb_movie.classes_, range(len(self._mlb_movie.classes_))))
+
+        self._user_encoder = LabelEncoder().fit(self.get_users())
+        self._genre_encoder = LabelEncoder().fit(self.get_genres())
+
+    @property
+    def n_users(self):
+        """Return the number of users in the dataset"""
+        return self._n_users
+
+    @property
+    def n_movies(self):
+        """Return the number of movies in the dataset"""
+        return self._n_movies
+
+    @property
+    def n_genres(self):
+        """Return the number of genres in the dataset"""
+        return self._n_genres
+
+    def get_user_train_movie_index(self, user_id):
+        """
+        Get the converted index of user movies
+        """
+        unique_user_train_movies = self.train_ratings.movieId[self.train_ratings.userId == user_id].unique()
+        return [self._movie_class_to_index[i]
+                            for i in unique_user_train_movies]
+
+    def get_user_test_movie_index(self, user_id):
+        """
+        Get the converted index of user movies
+        """
+        unique_user_test_movies = self.test_ratings.movieId[self.test_ratings.userId == user_id].unique()
+        return [self._movie_class_to_index[i]
+                            for i in unique_user_test_movies]
+
+    def get_user_cv_movie_index(self, user_id):
+        """
+        Get the converted index of user events
+        """
+        unique_user_cv_movies = self.cv_ratings.movieId[self.cv_ratings.userId == user_id].unique()
+        return [self._movie_class_to_index[i]
+                            for i in unique_user_cv_movies]
+    
+    def get_user_index(self, user_id):
+        """
+        Get user index
+        """
+        return self._user_encoder.transform([user_id])
+
+    def get_users(self):
+        return self.ratings.userId.unique()
+
+    def get_movies(self):
+        return self.ratings.movieId.unique()
+
+    def get_genres(self):
+        genre_set = set()
+        genres_list = self.ratings.genres
+        for genre in genres_list:
+            split_genre = genre.split('|')
+            for g in split_genre:
+                genre_set.add(g)
+        
+        return list(genre_set)
+
+    def get_train_users(self):
+        return self.train_ratings.userId.unique()
+
+    def get_test_users(self):
+        return self.test_ratings.userId.unique()
+
+    def get_cv_users(self):
+        return self.cv_ratings.userId.unique()
+
+    def get_train_movies(self):
+        return self.train_ratings.movieId.unique()
+
+    def get_test_movies(self):
+        return self.test_ratings.movieId.unique()
+
+    def get_user_train_genres(self, user_id):
+        """
+        Get train user genre indexes
+        """
+        genre_set = set()
+        genres_list = self.train_ratings[self.train_ratings.userId == user_id].genres
+        for genre in genres_list:
+            split_genre = genre.split('|')
+            for g in split_genre:
+                genre_set.add(g)
+        
+        return self._genre_encoder.transform(list(genre_set))
+
+    def get_user_unique_test_movies(self, user_id):
+        return self.test_ratings.movieId[self.test_ratings.userId == user_id].unique()
+
+
+    def get_user_train_movies(self, user_id, negative_count, corrupt_ratio):
+        """
+        Calls the get_user_movies method with the training data
+        """
+        return self.get_user_movies(user_id, self.train_ratings, negative_count, corrupt_ratio)
+
+
+    def get_user_test_movies(self, user_id):
+        """
+        Calls the get_user_movies method with the test data
+        """
+        return self.get_user_movies(user_id, self.test_ratings)
+
+    def get_user_movies(self, user_id, df, negative_count=0, corrupt_ratio=0):
+        """
+        This will get a single users movies (training or test based on input parameter).
+        We encode each user with a k-hot encoding, where a 1 if they have rated the item.
+        We then sample negative items they have not observed, if neg_ratio > 0.
+        Negative items have a target of 0 and positives 1.
+        We finally corrupt all the encoded user vectors, if the corrupt_ratio > 0.
+
+        :param user_id: user id in dataframe
+        :param df: the dataframe for training or test
+        :param negative_count: int, ratio of negative samples
+        :param corrupt_ratio: float, [0, 1] the probability of corrupting samples
+        :returns: Encoded User Vector, Y Target, item ids
+        """
+        # Get positive samples
+        positives = df.movieId[df.userId == user_id].unique().tolist()
+
+        # Testing...
+        if negative_count == 0:
+            return self._mlb_movie.transform([positives]).tocoo(), None, None
+
+        x, y, items = [], [], []
+
+        # For each positive we have N negatives
+        # i.e. len(positives) * (neg_count+1)
+        for i in range(len(positives)):
+            # Add a negative example
+            for j in range(negative_count):
+                neg = self._sample_negative_new(positives, self._mlb_movie.classes_)
+                y.append(0) # Negative Target
+                items.append(self._movie_class_to_index[neg]) # Negative index
+                x.append(positives + [neg]) # add negative pair
+            y.append(1)
+            x.append(positives)
+            items.append(self._movie_class_to_index[positives[i]])
+
+        x = self._mlb_movie.transform(x).tocoo()
+
+        # Sparse Matrix; directly take the data and corrupt it
+        if corrupt_ratio > 0:
+            x.data = self.corrupt_input(x.data, corrupt_ratio).astype(np.float32)
+
+        return x.astype(np.float32), np.array(y, dtype=np.float32), items
+
+
+    def sample_negative(self, pos_item_map, max_items):
+        """Sample uniformly items that are not observed
+
+        :param pos_item_map: set/list, listing all of the users observed items
+        :param max_items: int, item count
+        :returns: int negative item id
+        """
+        while True:
+            sample = np.random.randint(max_items)
+            if sample in pos_item_map:
+                continue
+            return sample
+
+    def _sample_negative_new(self, pos_items, all_items):
+        """Sample uniformly items that are not observed
+
+        :param pos_item_map: set/list, listing all of the users observed items
+        :param max_items: int, item count
+        :returns: int negative item id
+        """
+        while True:
+            sample = np.random.choice(all_items)
+            if sample in pos_items:
+                continue
+            return sample
+
+
+    def sample_negative_on_context(self, df, user_id, count):
+        """Sample uniformly items that are not observed
+
+        :param df: Dataframe to be sampled
+        :param user_id: user id for which the samples are to be provided
+        :param count: negative item count
+        :returns: int negative item id
+        """
+
+        return df[df.memberId != user_id].sample(count)
+
+
+    def corrupt_input(self, x, q):
+        """
+        Corrupt x with probability p by setting it to 0
+        else scale it by 1 / (1-p)
+        """
+        assert x.ndim == 1
+        scale = 1.0 / (1.0-q)
+        # Probability to remove it
+        # p = 1; 1-p = 0
+        p = 1-q
+        mask = bernoulli.rvs(p, size=x.shape[0])
+        # Mask outputs
+        x = x * mask
+        # Re-scale values
+        return x * scale
+
+
+    def _get_batch(self, df, user_ids, mlb):
+        """
+        This method creates a single vector for each user where all of their
+        events are set to a 1, otherwise its a 0.
+
+        In this case, this user has observed events: 1 and 4
+        [1, 0, 0, 1, 0]
+
+        TODO: Should probably abstract this out somewhere
+
+        :param df: pd.DataFrame of test/train
+        :param user_ids: list of user ids, this will query the dataframe
+        :param mlb: sklearn.MultiLabelBinarizer fitted with the event data
+        :returns: np.array of values
+        """
+        item_ids = [df.movieId[df.movieId == uid].unique() for uid in user_ids
+                    if len(df.movieId[df.userId == uid].unique()) > 0]
+        return mlb.transform(item_ids), item_ids
+
+
+def main():
+    print("main method")
+    ratings = MovieRatingsData()
+    print(ratings.get_genres())
+
+if __name__ == '__main__':
+    main()
---
+++ src/aeer/model/attn_movie_auto_encoder.py
@@ -0,0 +1,384 @@
+'''
+Denoising AutoEncoder Implementation that utilizes the attention mechanism
+to incorporate contextual information such as the genre for the movielens dataset
+'''
+import argparse
+import os
+
+import aeer.dataset.movie_dataset as ds
+import tensorflow as tf
+
+import numpy as np
+from sklearn.utils import shuffle
+
+from aeer.model.utils import ACTIVATION_FN, set_logging_config
+
+"""
+Example Usage:
+
+Run for 20 epochs, 100 hidden units and a 0.5 corruption ratio
+python attention_auto_encoder.py --epochs 20 --size 100 --corrupt 0.5
+
+To turn off latent factors, eg for group latent factor
+python attention_auto_encoder.py --nogroup
+"""
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-g', '--gpu', help='set gpu device number 0-3', type=str, default='3')
+parser.add_argument('-e', '--epochs', help='Number of epochs', type=int, default=5)
+parser.add_argument('-s', '--size', help='Number of hidden layer',
+                    type=int, default=100)
+parser.add_argument('-n', '--neg_count', help='Number of negatives', type=int,
+                    default=4)
+parser.add_argument('-c', '--corrupt', help='Corruption ratio', type=float,
+                    default=0.1)
+parser.add_argument('--save_dir', help='Directory to save the model; if not set will not save', type=str, default=None)
+# Pass the Flag to disable
+parser.add_argument('--nogenre', help='disable genre latent factor', action="store_true")
+
+activation_fn_names = ACTIVATION_FN.keys()
+parser.add_argument('--hidden_fn',
+                    help='hidden activation function to use',
+                    default='relu', type=str, choices=activation_fn_names)
+
+parser.add_argument('--output_fn',
+                    help='output activation function to use',
+                    default='sigmoid', type=str, choices=activation_fn_names)
+
+
+class AttentionMovieAutoEncoder(object):
+
+    def __init__(self, n_inputs, n_hidden, n_outputs, n_genres,
+                 hidden_activation='relu', output_activation='sigmoid',
+                 learning_rate=0.001):
+        """
+
+        :param n_inputs: int, Number of input features (number of events)
+        :param n_hidden: int, Number of hidden units
+        :param n_outputs: int, Number of output features (number of events)
+        :param n_genres: int, Number of genres or None to disable
+        :param learning_rate: float, Step size
+        """
+
+        self.x = tf.placeholder(tf.float32, shape=[None, n_inputs])
+        self.genre_id = tf.placeholder(tf.int32, shape=[None])
+
+        # We need to gather the indices from the matrix where our outputs are
+        self.gather_indices = tf.placeholder(tf.int32, shape=[None, 2])
+
+        self.y = tf.placeholder(tf.float32, shape=[None])
+        self.dropout = tf.placeholder_with_default(1.0, shape=(), name='Dropout')
+
+        reg_constant = 0.01
+        # Weights
+        W = tf.get_variable('W', shape=[n_inputs, n_hidden], regularizer=tf.contrib.layers.l2_regularizer(scale=reg_constant))
+        b = tf.get_variable('Bias', shape=[n_hidden])
+
+        # Uniform Initialization U(-eps, eps)
+        eps = 0.01
+        preactivation = tf.nn.xw_plus_b(self.x, W, b)
+        hidden = ACTIVATION_FN[hidden_activation](preactivation)
+        hidden = tf.nn.dropout(hidden, self.dropout)
+
+        attention = hidden
+        # setup attention mechanism
+        # Add group latent factor
+        if n_genres is not None:
+            genre_bias = tf.get_variable('GenreBias', shape=[n_genres, n_hidden],
+                                         initializer=tf.random_uniform_initializer(-eps, eps))
+            self.genre_factor = tf.nn.embedding_lookup(genre_bias, self.genre_id,
+                                                       name='GenreLookup')
+            g_attn_weight = tf.get_variable('AttentionWLogits',
+                                                      shape=[n_hidden, 1], regularizer=tf.contrib.layers.l2_regularizer(scale=reg_constant))
+            g_attention = tf.matmul(self.genre_factor, g_attn_weight)
+
+            # Weighted sum of genre factors
+            genre_weighted = tf.reduce_sum(g_attention * self.genre_factor,
+                                      axis=0)
+            # Add to
+            attention += tf.squeeze(genre_weighted)
+
+        attn_output = tf.nn.softmax(tf.nn.tanh(attention))
+
+        # create the output layer
+        W2 = tf.get_variable('W2', shape=[n_hidden, n_outputs], regularizer=tf.contrib.layers.l2_regularizer(scale=reg_constant))
+        b2 = tf.get_variable('Bias2', shape=[n_outputs])
+        preactivation_output = tf.nn.xw_plus_b(tf.multiply(attn_output, hidden), W2, b2)
+        self.outputs = ACTIVATION_FN[output_activation](preactivation_output)
+
+        self.targets = tf.gather_nd(self.outputs, self.gather_indices)
+        self.actuals = tf.placeholder(tf.int64, shape=[None])
+
+        # square loss
+        self.loss = tf.losses.mean_squared_error(self.targets, self.y)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        # Train Model
+        self.train = optimizer.minimize(self.loss)
+
+def precision_at_k(predictions, actuals, k):
+    """
+    Computes the precision at k
+    :param predictions: array, predicted values
+    :param actuals: array, actual values
+    :param k: int, value to compute the metric at
+    :returns precision: float, the precision score at k
+    """
+    N = len(actuals)
+    hits = len(set(predictions[-k:]).intersection(set(actuals)))
+    precision = hits / min(N, k)
+    return precision
+
+def recall_at_k(predictions, actuals, k):
+    """
+    Computes the recall at k
+    :param predictions: array, predicted values
+    :param actuals: array, actual values
+    :param k: int, value to compute the metric at
+    :returns recall: float, the recall score at k
+    """
+    N = len(actuals)
+    hits = len(set(predictions[-k:]).intersection(set(actuals)))
+    recall = hits / N
+    return recall
+
+def map_at_k(predictions, actuals, k):
+    """
+    Computes the MAP at k
+    :param predictions: array, predicted values
+    :param actuals: array, actual values
+    :param k: int, value to compute the metric at
+    :returns MAP: float, the score at k
+    """
+    avg_prec = []
+    for i in range(1, k+1):
+        prec = precision_at_k(predictions, actuals, i)
+        avg_prec.append(prec)
+    return np.mean(avg_prec)
+
+def ndcg_at_k(predictions, actuals, k):
+    """
+    Computes the NDCG at k
+    :param predictions: array, predicted values
+    :param actuals: array, actual values
+    :param k: int, value to compute the metric at
+    :returns NDCG: float, the score at k
+    """
+    N = min(len(actuals), k)
+    cum_gain = 0
+    ideal_gain = 0
+    topk = predictions[-N:]
+    hits = 0
+    # calculate the ideal gain at k
+    for i in range(0, N):
+        if topk[i] in actuals:
+            cum_gain += 1 / np.log2(i + 2)
+            hits = hits + 1
+
+    for i in range(0, hits):
+        ideal_gain += 1 / np.log2(i + 2)
+    if ideal_gain != 0:
+        ndcg = cum_gain / ideal_gain
+    else:
+        ndcg = 0
+    return ndcg
+
+def main():
+    n_epochs = FLAGS.epochs
+    n_hidden = FLAGS.size
+    NEG_COUNT = FLAGS.neg_count
+    CORRUPT_RATIO = FLAGS.corrupt
+
+    ratings_data = ds.MovieRatingsData()
+    users = ratings_data.get_train_users()
+
+    n_inputs = ratings_data.n_movies
+    n_genres = ratings_data.n_genres
+    n_outputs = ratings_data.n_movies
+
+    # We set to None to turn off the genre latent factors
+    if FLAGS.nogenre:
+        print("Disabling Genre Latent Factor")
+        n_genres = None
+
+    model = AttentionMovieAutoEncoder(n_inputs, n_hidden, n_outputs, n_genres,
+                                    FLAGS.hidden_fn, FLAGS.output_fn,
+                                    learning_rate=0.001)
+    tf_config = tf.ConfigProto(
+        gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.25,
+                                  allow_growth=True))
+    sv = tf.train.Supervisor(logdir=FLAGS.save_dir)
+    set_logging_config(FLAGS.save_dir)
+
+    with sv.prepare_or_wait_for_session(config=tf_config) as sess:
+        prev_epoch_loss = 0.0
+        for epoch in range(n_epochs):
+            # additive gaussian noise or multiplicative mask-out/drop-out noise
+            epoch_loss = 0.0
+            users = shuffle(users)
+            precision = []
+            recall = []
+            mean_avg_prec = []
+            ndcg = []
+            eval_at = [5, 10]
+
+            tf.logging.info("Training the model...")
+            for user_id in users:
+                x, y, item = ratings_data.get_user_train_movies(
+                                                    user_id, NEG_COUNT, CORRUPT_RATIO)
+                train_movie_index = ratings_data.get_user_train_movie_index(user_id)
+                genre_ids = ratings_data.get_user_train_genres(user_id)
+
+                # We only compute loss on events we used as inputs
+                # Each row is to index the first dimension
+                gather_indices = list(zip(range(len(y)), item))
+
+                # Get a batch of data
+                batch_loss, _ = sess.run([model.loss, model.train], {
+                    model.x: x.toarray().astype(np.float32),
+                    model.gather_indices: gather_indices,
+                    model.genre_id: genre_ids,
+                    model.y: y,
+                    model.dropout: 0.8
+                })
+                epoch_loss += batch_loss
+            tf.logging.info("Epoch: {:>16}       Loss: {:>10,.6f}".format("%s/%s" % (epoch, n_epochs),
+                                                                epoch_loss))
+            tf.logging.info("")
+            if prev_epoch_loss != 0 and abs(epoch_loss - prev_epoch_loss) < 1:
+                tf.logging.info("Decaying learning rate...")
+                model.decay_learning_rate(sess, 0.5)
+
+        # evaluate the model on the cv set
+        cv_users = ratings_data.get_cv_users()
+        precision = []
+        recall = []
+        mean_avg_prec = []
+        ndcg = []
+        eval_at = [5, 10]
+
+        tf.logging.info("Evaluating on the CV set...")
+        valid_test_users = 0
+        for user_id in cv_users:
+            # check if user was present in training data
+            train_users = ratings_data.get_train_users()
+            if user_id in train_users:
+                valid_test_users = valid_test_users + 1
+                test_movie_index = ratings_data.get_user_cv_movie_index(user_id)
+
+                x, _, _ = ratings_data.get_user_train_movies(user_id, 0, 0)
+                genre_ids = ratings_data.get_user_train_genres(user_id)
+                # Compute score
+                score = sess.run(model.outputs, {
+                    model.x: x.toarray().astype(np.float32),
+                    model.genre_id: genre_ids,
+                    model.dropout: 1.0
+                })[0] # We only do one sample at a time, take 0 index
+
+                # Sorted in ascending order, we then take the last values
+                index = np.argsort(score)
+
+                # Number of test instances
+                preck = []
+                recallk = []
+                mapk = []
+                ndcgk = []
+                for k in eval_at:
+                    preck.append(precision_at_k(index, test_movie_index, k))
+                    recallk.append(recall_at_k(index, test_movie_index, k))
+                    mapk.append(map_at_k(index, test_movie_index, k))
+                    ndcgk.append(ndcg_at_k(index, test_movie_index, k))
+
+                precision.append(preck)
+                recall.append(recallk)
+                mean_avg_prec.append(mapk)
+                ndcg.append(ndcgk)
+
+        if valid_test_users > 0:
+            # Unpack the lists zip(*[[1,2], [3, 4]]) => [1,3], [2,4]
+            avg_precision_5, avg_precision_10 = zip(*precision)
+            avg_precision_5, avg_precision_10 = np.mean(avg_precision_5), np.mean(avg_precision_10)
+
+            avg_recall_5, avg_recall_10 = zip(*recall)
+            avg_recall_5, avg_recall_10 = np.mean(avg_recall_5), np.mean(avg_recall_10)
+
+            avg_map_5, avg_map_10 = zip(*mean_avg_prec)
+            avg_map_5, avg_map_10 = np.mean(avg_map_5), np.mean(avg_map_10)
+
+            avg_ndcg_5, avg_ndcg_10 = zip(*ndcg)
+            avg_ndcg_5, avg_ndcg_10 = np.mean(avg_ndcg_5), np.mean(avg_ndcg_10)
+
+        # Directly access variables
+        tf.logging.info(f"Precision@5: {avg_precision_5:>10.6f}       Precision@10: {avg_precision_10:>10.6f}")
+        tf.logging.info(f"Recall@5:    {avg_recall_5:>10.6f}       Recall@10:    {avg_recall_10:>10.6f}")
+        tf.logging.info(f"MAP@5:       {avg_map_5:>10.6f}       MAP@10:       {avg_map_10:>10.6f}")
+        tf.logging.info(f"NDCG@5:      {avg_ndcg_5:>10.6f}       NDCG@10:      {avg_ndcg_10:>10.6f}")
+        tf.logging.info("")
+        
+        # evaluate on test users
+        tf.logging.info("Evaluating on the test set...")
+        valid_test_users = 0
+        precision = []
+        recall = []
+        mean_avg_prec = []
+        ndcg = []
+        eval_at = [5, 10]
+        for user_id in ratings_data.get_test_users():
+            # check if user was present in training data
+            train_users = ratings_data.get_train_users()
+            if user_id in train_users:
+                valid_test_users = valid_test_users + 1
+                test_movie_index = ratings_data.get_user_test_movie_index(user_id)
+
+                x, _, _ = ratings_data.get_user_train_movies(user_id, 0, 0)
+                genre_ids = ratings_data.get_user_train_genres(user_id)
+                # Compute score
+                score = sess.run(model.outputs, {
+                    model.x: x.toarray().astype(np.float32),
+                    model.genre_id: genre_ids,
+                    model.dropout: 1.0
+                })[0] # We only do one sample at a time, take 0 index
+
+                # Sorted in ascending order, we then take the last values
+                index = np.argsort(score)
+
+                # Number of test instances
+                preck = []
+                recallk = []
+                mapk = []
+                ndcgk = []
+                for k in eval_at:
+                    preck.append(precision_at_k(index, test_movie_index, k))
+                    recallk.append(recall_at_k(index, test_movie_index, k))
+                    mapk.append(map_at_k(index, test_movie_index, k))
+                    ndcgk.append(ndcg_at_k(index, test_movie_index, k))
+
+                precision.append(preck)
+                recall.append(recallk)
+                mean_avg_prec.append(mapk)
+                ndcg.append(ndcgk)
+
+        if valid_test_users > 0:
+            # Unpack the lists zip(*[[1,2], [3, 4]]) => [1,3], [2,4]
+            avg_precision_5, avg_precision_10 = zip(*precision)
+            avg_precision_5, avg_precision_10 = np.mean(avg_precision_5), np.mean(avg_precision_10)
+
+            avg_recall_5, avg_recall_10 = zip(*recall)
+            avg_recall_5, avg_recall_10 = np.mean(avg_recall_5), np.mean(avg_recall_10)
+
+            avg_map_5, avg_map_10 = zip(*mean_avg_prec)
+            avg_map_5, avg_map_10 = np.mean(avg_map_5), np.mean(avg_map_10)
+
+            avg_ndcg_5, avg_ndcg_10 = zip(*ndcg)
+            avg_ndcg_5, avg_ndcg_10 = np.mean(avg_ndcg_5), np.mean(avg_ndcg_10)
+
+        # Directly access variables
+        tf.logging.info(f"Precision@5: {avg_precision_5:>10.6f}       Precision@10: {avg_precision_10:>10.6f}")
+        tf.logging.info(f"Recall@5:    {avg_recall_5:>10.6f}       Recall@10:    {avg_recall_10:>10.6f}")
+        tf.logging.info(f"MAP@5:       {avg_map_5:>10.6f}       MAP@10:       {avg_map_10:>10.6f}")
+        tf.logging.info(f"NDCG@5:      {avg_ndcg_5:>10.6f}       NDCG@10:      {avg_ndcg_10:>10.6f}")
+        tf.logging.info("")
+    sv.request_stop()
+
+if __name__ == '__main__':
+    FLAGS = parser.parse_args()
+    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+    main()
